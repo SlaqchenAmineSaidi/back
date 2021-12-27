996d63c97187e7141b2b9cb9a9b499ff847505f7
slaqchen <ami0190ne@gmail.com>
2021-12-27 01:57:29 +0100

    second

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/sanctum/token', [App\Http\Controllers\UserController::class, 'login']);
+Route::post('/sanctum/token/store', [App\Http\Controllers\UserController::class, 'store']);
 
-Route::apiResource('/user', App\Http\Controllers\UserController::class)->only('store');
+//Route::apiResource('/user', App\Http\Controllers\UserController::class)->only('store');
 
 Route::middleware('auth:sanctum')->get('/user/revoke', function (Request $request) {
     $user = $request->user();
