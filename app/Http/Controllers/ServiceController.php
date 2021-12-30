e8ce3ca5bc40c8f512cb8da16c1c158894909c65
slaqchen <ami0190ne@gmail.com>
2021-12-31 00:39:34 +0100

    second

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reservation;
 use App\Models\Service;
 use Illuminate\Http\Request;
 
@@ -41,7 +42,7 @@ class ServiceController extends Controller
             'price'=> 'required',
         ]);
         $service = Service::create([
-            'price'=> $request->reservation_id,
+            'reservation_id'=> $request->reservation_id,
             'title'=> $request->title,
             'price'=> $request->price,
         ]);
